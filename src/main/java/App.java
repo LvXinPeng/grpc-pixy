f0a69a0819f0f7370b32da240624db2ae3636a32
xinplv <xiaopeng1220>
2020-08-20 17:36:33 +0800

    Revert "add code listTopics"

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,6 @@
-import com.csvw.lib.*;
+import com.csvw.lib.KafkaPixyServiceGrpc;
+import com.csvw.lib.ProdRq;
+import com.csvw.lib.ProdRs;
 import com.google.protobuf.ByteString;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
@@ -15,7 +17,7 @@ public class App {
     private static String targetHost = "10.122.70.102";
     private static int targetPort = 19091;
 
-    private static void produceMsg(String cluster, String topic, String msg) {
+    public static void produceMsg(String cluster, String topic, String msg) {
         final Logger logger = Logger.getLogger(App.class.getName());
         //ManagedChannel channel = ManagedChannelBuilder.forTarget("10.122.70.102:19091").usePlaintext().build();
         ManagedChannel channel = ManagedChannelBuilder.forAddress(targetHost, targetPort).usePlaintext().build();
@@ -24,13 +26,8 @@ public class App {
         logger.info(stub.toString());
 
         ByteString bytesMsg = ByteString.copyFromUtf8(msg);
-        ProdRq rq = ProdRq.newBuilder()
-                .setCluster(cluster)
-                .setTopic(topic)
-                .setMessage(bytesMsg)
-                .setKeyUndefined(true)
-                .setAsyncMode(true)
-                .build();
+        ProdRq rq = ProdRq.newBuilder().setCluster(cluster).setTopic(topic).setMessage(bytesMsg)
+                .setKeyUndefined(true).setAsyncMode(true).build();
         logger.info(rq.toString());
 
         StreamObserver<ProdRs> rsStreamObserver = new StreamObserver<ProdRs>() {
@@ -56,45 +53,13 @@ public class App {
         stub.produce(rq, rsStreamObserver);
     }
 
-    private static void listTopicSet(String cluster) {
-        final Logger logger = Logger.getLogger(App.class.getName());
-        //ManagedChannel channel = ManagedChannelBuilder.forTarget("10.122.70.102:19091").usePlaintext().build();
-        ManagedChannel channel = ManagedChannelBuilder.forAddress(targetHost, targetPort).usePlaintext().build();
-        logger.info(channel.toString());
-        KafkaPixyServiceGrpc.KafkaPixyServiceStub stub = KafkaPixyServiceGrpc.newStub(channel);
-
-        ListTopicRq topicRq = ListTopicRq.newBuilder().setCluster(cluster).build();
-        StreamObserver<ListTopicRs> topicRsStreamObserver = new StreamObserver<ListTopicRs>() {
-            @Override
-            public void onNext(ListTopicRs listTopicRs) {
-
-                logger.info("next ------");
-                logger.info(String.valueOf(listTopicRs.getTopicsCount()));
-            }
-
-            @Override
-            public void onError(Throwable throwable) {
-
-            }
-
-            @Override
-            public void onCompleted() {
-
-            }
-        };
-
-        stub.listTopics(topicRq, topicRsStreamObserver);
-    }
-
     public static void main(String[] args) {
 
-        String cluster = "default";
-        String cluster2 = "10.160.242.166:9092";
-        String topic = "kafka-pixy2";
+        String cluster = "default_cluster";
+        String topic = "kafka-pixy";
         String msg = "hellokafkapixy";
 
         produceMsg(cluster, topic, msg);
 
-        listTopicSet(cluster2);
     }
 }
