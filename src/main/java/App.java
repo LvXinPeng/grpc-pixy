dd2a05cbf6a563ffc8bd13322c9ff1458d00e92a
xinplv <xiaopeng1220>
2020-08-27 16:37:29 +0800

    调整代码结构，封装同步/异步发送消息

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,100 +1,29 @@
-import com.csvw.lib.*;
-import com.google.protobuf.ByteString;
-import io.grpc.ManagedChannel;
-import io.grpc.ManagedChannelBuilder;
-import io.grpc.stub.StreamObserver;
-
-import java.util.logging.Logger;
+import com.csvw.KafkaPixyClient;
 
 /**
- * Hello world!
+ * Hello Kafka-pixy!
  */
 public class App {
 
 
-    private static String targetHost = "10.122.70.102";
-    private static int targetPort = 19091;
-
-    private static void produceMsg(String cluster, String topic, String msg) {
-        final Logger logger = Logger.getLogger(App.class.getName());
-        //ManagedChannel channel = ManagedChannelBuilder.forTarget("10.122.70.102:19091").usePlaintext().build();
-        ManagedChannel channel = ManagedChannelBuilder.forAddress(targetHost, targetPort).usePlaintext().build();
-        logger.info(channel.toString());
-        KafkaPixyServiceGrpc.KafkaPixyServiceStub stub = KafkaPixyServiceGrpc.newStub(channel);
-        logger.info(stub.toString());
-
-        ByteString bytesMsg = ByteString.copyFromUtf8(msg);
-        ProdRq rq = ProdRq.newBuilder()
-                .setCluster(cluster)
-                .setTopic(topic)
-                .setMessage(bytesMsg)
-                .setKeyUndefined(true)
-                .setAsyncMode(true)
-                .build();
-        logger.info(rq.toString());
-
-        StreamObserver<ProdRs> rsStreamObserver = new StreamObserver<ProdRs>() {
-            @Override
-            public void onNext(ProdRs prodRs) {
-                long offset = prodRs.getOffset();
-                int partition = prodRs.getPartition();
-                logger.info("next ------");
-                logger.info(String.valueOf(offset));
-                logger.info(String.valueOf(partition));
-            }
-
-            @Override
-            public void onError(Throwable throwable) {
-                logger.info("error ------");
-            }
-
-            @Override
-            public void onCompleted() {
-                logger.info("complete ------");
-            }
-        };
-        stub.produce(rq, rsStreamObserver);
-    }
-
-    private static void listTopicSet(String cluster) {
-        final Logger logger = Logger.getLogger(App.class.getName());
-        //ManagedChannel channel = ManagedChannelBuilder.forTarget("10.122.70.102:19091").usePlaintext().build();
-        ManagedChannel channel = ManagedChannelBuilder.forAddress(targetHost, targetPort).usePlaintext().build();
-        logger.info(channel.toString());
-        KafkaPixyServiceGrpc.KafkaPixyServiceStub stub = KafkaPixyServiceGrpc.newStub(channel);
-
-        ListTopicRq topicRq = ListTopicRq.newBuilder().setCluster(cluster).build();
-        StreamObserver<ListTopicRs> topicRsStreamObserver = new StreamObserver<ListTopicRs>() {
-            @Override
-            public void onNext(ListTopicRs listTopicRs) {
-
-                logger.info("next ------");
-                logger.info(String.valueOf(listTopicRs.getTopicsCount()));
-            }
-
-            @Override
-            public void onError(Throwable throwable) {
-
-            }
-
-            @Override
-            public void onCompleted() {
-
-            }
-        };
-
-        stub.listTopics(topicRq, topicRsStreamObserver);
-    }
-
     public static void main(String[] args) {
 
+        String target = "10.122.70.102:19091";
         String cluster = "default";
-        String cluster2 = "10.160.242.166:9092";
-        String topic = "kafka-pixy2";
-        String msg = "hellokafkapixy";
+        String topic = "kafka-pixy";
+        String topicAsync = "kafka-pixy2";
+        String msg = "hello kafka pixy-------sync------";
+        String msgAsync = "hello kafka pixy-------async------";
 
-        produceMsg(cluster, topic, msg);
+        // 客户端同步发送消息
+        KafkaPixyClient client = new KafkaPixyClient(target);
+        client.produceMsg(cluster, topic, msg, true);
+        client.listTopics(cluster, false);
 
-        listTopicSet(cluster2);
+        // 客户端异步发送消息
+        KafkaPixyClient clientAsync = new KafkaPixyClient(target, true);
+        clientAsync.produceMsgAsync(cluster, topic, msgAsync, true);
+
+
     }
 }
---
+++ src/main/java/com/csvw/KafkaPixyClient.java
@@ -0,0 +1,139 @@
+package com.csvw;
+
+import com.google.protobuf.ByteString;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.StatusRuntimeException;
+import io.grpc.stub.StreamObserver;
+import mailgun.kafkapixy.*;
+
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * 创建一个Kafka Pixy Client客户端对象
+ */
+public class KafkaPixyClient {
+    private final Logger logger = Logger.getLogger(KafkaPixyClient.class.getName());
+
+    private KafkaPixyGrpc.KafkaPixyBlockingStub stub;
+    private KafkaPixyGrpc.KafkaPixyStub stubAsync;
+    private Boolean isAsync;
+
+    public KafkaPixyClient(String target) {
+
+        ManagedChannel channel = ManagedChannelBuilder.forTarget(target).usePlaintext().build();
+        this.stub = KafkaPixyGrpc.newBlockingStub(channel);
+    }
+
+    public KafkaPixyClient(String target, Boolean isAsync) {
+
+        ManagedChannel channel = ManagedChannelBuilder.forTarget(target).usePlaintext().build();
+        this.stubAsync = KafkaPixyGrpc.newStub(channel);
+        this.isAsync = isAsync;
+    }
+
+
+    /**
+     * 同步发送消息
+     *
+     * @param cluster
+     * @param topic
+     * @param msg
+     * @param isPartitionedByHash 消息是否哈希分区
+     */
+    public void produceMsg(String cluster, String topic, String msg, Boolean isPartitionedByHash) {
+
+        RecordHeader recordHeader = RecordHeader.newBuilder().build();
+        ByteString bytesMsg = ByteString.copyFromUtf8(msg);
+        ProdRq rq = ProdRq.newBuilder()
+                .setCluster(cluster)
+                .setTopic(topic)
+                .setMessage(bytesMsg)
+                .setKeyUndefined(!isPartitionedByHash)
+                .setAsyncMode(false)
+                .addHeaders(recordHeader)
+                .build();
+        logger.info("Client sending {" + rq.toString() + "}");
+
+        try {
+            stub.produce(rq);
+        } catch (StatusRuntimeException e) {
+            logger.log(Level.WARNING, "RPC failed: {0}", e.getStatus());
+        }
+    }
+
+    /**
+     * 异步发送消息
+     *
+     * @param cluster
+     * @param topic
+     * @param msg
+     * @param isPartitionedByHash 消息是否哈希分区
+     */
+    public void produceMsgAsync(String cluster, String topic, String msg, Boolean isPartitionedByHash) {
+
+        RecordHeader recordHeader = RecordHeader.newBuilder().build();
+        ByteString bytesMsg = ByteString.copyFromUtf8(msg);
+        ProdRq rq = ProdRq.newBuilder()
+                .setCluster(cluster)
+                .setTopic(topic)
+                .setMessage(bytesMsg)
+                .setKeyUndefined(!isPartitionedByHash)
+                .setAsyncMode(true)
+                .addHeaders(recordHeader)
+                .build();
+        logger.info("Client sending {" + rq.toString() + "}");
+        StreamObserver<ProdRs> rsStreamObserver = new StreamObserver<ProdRs>() {
+
+            @Override
+            public void onNext(ProdRs prodRs) {
+                long offset = prodRs.getOffset();
+                int partition = prodRs.getPartition();
+                logger.info("offset:" + String.valueOf(offset));
+                logger.info("partition:" + String.valueOf(partition));
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                logger.warning("produceMsgAsync error ------");
+            }
+
+            @Override
+            public void onCompleted() {
+                logger.info("produceMsgAsync complete ------");
+            }
+        };
+        try {
+            stubAsync.produce(rq, rsStreamObserver);
+        } catch (StatusRuntimeException e) {
+            logger.log(Level.WARNING, "RPC failed: {0}", e.getStatus());
+        }
+    }
+
+    /**
+     * 获取kafka-pixy代理下某个集群所有的topic
+     *
+     * @param cluster
+     * @param isWithPartitions topic信息是否带元数据(分区信息)
+     * @return
+     */
+    public Map<String, GetTopicMetadataRs> listTopics(String cluster, Boolean isWithPartitions) {
+
+        ListTopicRq topicRq = ListTopicRq.newBuilder()
+                .setCluster(cluster)
+                .setWithPartitions(isWithPartitions)
+                .build();
+
+        try {
+            ListTopicRs listTopicRs = stub.listTopics(topicRq);
+            return listTopicRs.getTopicsMap();
+
+        } catch (StatusRuntimeException e) {
+            logger.log(Level.WARNING, "RPC failed: {0}", e.getStatus());
+            return null;
+        }
+    }
+
+}
